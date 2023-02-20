720dfa26510372ca3deb32e9dbe0fa167fbfd56e
olittobee <olittobee4u@gmail.com>
2023-02-20 10:46:59 -0800

    task09

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+
+}
