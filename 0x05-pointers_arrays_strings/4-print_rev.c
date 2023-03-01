6022a9f9b6700212170a018a71354ff7df864cb3
olittobee <olittobee4u@gmail.com>
2023-03-01 07:55:50 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string to be printed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+
+	while (s[b] != '\0')
+		b++;
+	for (a = b - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+	_putchar('\n');
+}
