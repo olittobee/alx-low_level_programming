8eb5d2ea635fb44951e5af5c2c79226be7cea443
olittobee <olittobee4u@gmail.com>
2023-03-01 04:18:12 -0800

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value of a var using pointers
+ *
+ * @n: int to upddate
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
