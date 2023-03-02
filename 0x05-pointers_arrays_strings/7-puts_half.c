aba7e9aecc0c5d0d0c720fc82682be7d07396bb5
olittobee <olittobee4u@gmail.com>
2023-03-02 05:48:34 -0800

    task07

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,19 +9,19 @@
 void puts_half(char *str)
 {
 	int i;
-	int len = 0;
+	int length = 0;
 
 	for (i = 0 ; str[i] != '\0' ; i++)
 		len++;
-	if (len % 2 == 0)
+	if (length % 2 == 0)
 	{
-		for (i = len / 2 ; str[i] != '\0' ; i++)
+		for (i = length / 2 ; str[i] != '\0' ; i++)
 			_putchar(str[i]);
 		_putchar('\n');
 	}
-	else if (len % 2 != 0)
+	else if (length % 2 != 0)
 	{
-		for (i = (len - 1) / 2 ; str[i] != '\0' ; i++)
+		for (i = (length - 1) / 2 ; str[i] != '\0' ; i++)
 			_putchar(str[i]);
 		_putchar('\n');
 	}
