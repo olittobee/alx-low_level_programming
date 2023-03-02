c936ea46ff05702b2dd1ddccaba4d58f5a870b60
olittobee <olittobee4u@gmail.com>
2023-03-02 06:14:10 -0800

    osumi

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,27 +3,23 @@
 /**
  * puts_half - function to print half of the string
  *@str: function parameter
- * Return: Always 0.
+ * Return: Always 0
  */
 
 void puts_half(char *str)
 {
-	int i;
-	int length = 0;
+	int a, n, longi;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
-		length++;
-	if (length % 2 == 0)
-	{
-		for (i = length / 2 ; str[i] != '\0' ; i++)
-			_putchar(str[i]);
-		_putchar('\n');
-	}
-	else if (length % 2 != 0)
-	{
-		for (i = (length - 1) / 2 ; str[i] != '\0' ; i++)
-			_putchar(str[i]);
-		_putchar('\n');
-	}
+	longi = 0;
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
 
+	n = (longi / 2);
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+
+	_putchar('\n');
 }
