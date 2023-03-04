7b97b3adec6ca6501276acd8f221a4a73d250d62
olittobee <olittobee4u@gmail.com>
2023-03-04 08:27:56 -0800

    task07

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - prints a string int o 1337
+ * @n: input
+ *
+ * Return: return the value of n.
+ */
+
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+			}
+
+		}
+	}
+	return (n);
+}
