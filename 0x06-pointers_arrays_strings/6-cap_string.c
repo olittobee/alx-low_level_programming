41936bc72985b44844a9218e5717863ac583a050
olittobee <olittobee4u@gmail.com>
2023-03-04 07:37:19 -0800

    task06

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,24 +13,25 @@ char *cap_string(char *str)
 
 	while (str[x])
 	{
-	while (!(str[x] >= 'a' && str[x] <= 'z'))
+		while (!(str[x] >= 'a' && str[x] <= 'z'))
 
 			x++;
+
 		if (str[x - 1] == ' ' ||
-			str[x - 1] == '\t' ||
-			str[x - 1] == '\n' ||
-			str[x - 1] == ',' ||
-			str[x - 1] == ';' ||
-			str[x - 1] == '.' ||
-			str[x - 1] == '!' ||
-			str[x - 1] == '?' ||
-			str[x - 1] == '"' ||
-			str[x - 1] == '(' ||
-			str[x - 1] == ')' ||
-			str[x - 1] == '{' ||
-			str[x - 1] == '}' ||
-			x == 0)
-				str[x] -= 32;
+		str[x - 1] == '\t' ||
+		str[x - 1] == '\n' ||
+		str[x - 1] == ',' ||
+		str[x - 1] == ';' ||
+		str[x - 1] == '.' ||
+		str[x - 1] == '!' ||
+		str[x - 1] == '?' ||
+		str[x - 1] == '"' ||
+		str[x - 1] == '(' ||
+		str[x - 1] == ')' ||
+		str[x - 1] == '{' ||
+		str[x - 1] == '}' ||
+		x == 0)
+			str[x] -= 32;
 		x++;
 	}
 	return (str);
